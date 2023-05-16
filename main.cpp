994ca5ebb8d7577227c300e1bf040413d2cffd1d
Andaman6481331 <132637308+Andaman6481331@users.noreply.github.com>
2023-05-16 09:12:06 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,40 +1,35 @@
+
 #include <iostream>
+#include <string.h>
+#include <iomanip>
+
 using namespace std;
 
-int main(){
+int main(void){
+  char *pa, *pb, temp;
+  char a[1000];
+    cout<<"Input string:";
+    cin>>a;
+  int SIZE = strlen(a);
+  pa = &a[0];
+  cout<< "Original: ";
+  for (int i = 0; i < strlen(a) - 1; i++, pa++){
+    cout<<setw(3)<<*pa<<" ";
+  }
+    cout<<setw(3)<<*pa<<endl;
 
-  int a;
-  double d;
-  cout<<&a<<" size "<<sizeof(a) <<endl;
-  cout<<&d<<" size "<<sizeof(d) <<endl; 
-  
-  /*  int x,*p;
-    p=&x;
-    x=5;
-
-    cout<< x  <<" "<< *p <<endl;
-    cout<< &x <<" "<<  p <<endl;
-    cout<< &p <<endl;*/
-
-/*Example Array*/
-  /*
-int *ip;
-int a[ ] = {31415, 21828};
-ip = a;
-// alternative:	ip = &a[0];
-
-cout<<a<<endl;			
-cout<<ip<<" "<<*ip<<endl;	
-  
-(*ip)++; 
-cout<<a<<" "<<a[0]<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	
-  
-ip++; 
-cout<<a<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	// print 
-*/
- /*the address is 4324, not 4321
- because the pointer is incremented 						
- by the size of an integer (4 bytes) */
+    pa = &a[0]; pb = &a[strlen(a) - 1];
+    cout<<"Reverse: ";
+    for (int i = 0; i < strlen(a) / 2; i++) {
+        temp = *pa;
+        *pa = *pb;
+        *pb = temp;
+        pa++; pb--;
+    }
+    pa = &a[0];
+    for (int i = 0; i < strlen(a); i++,pa++){
+        cout<<setw(3)<<*pa<<" ";
+    }
+    
+return 0;
 }
